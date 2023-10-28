d125e4f5ecc74a81b2d8a9bc6ae4b41ceb6933c5
Tate Jenkins <jenkins.tate@proton.me>
2023-10-28 08:47:11 -0700

    nouislider

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import indexRouter from './routes/index.js';
 
 // Set up server
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Express middleware
 app.use(json());
